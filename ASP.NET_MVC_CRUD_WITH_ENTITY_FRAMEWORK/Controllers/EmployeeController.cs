b0151fbb36cd54aeeabd968a675459cb714e4a7d
Joshua C. Magoliman <magolimanjoshua22@gmail.com>
2020-06-26 14:41:19 +0800

    first commit

---
+++ ASP.NET_MVC_CRUD_WITH_ENTITY_FRAMEWORK/Controllers/EmployeeController.cs
@@ -0,0 +1,75 @@
+﻿using ASP.NET_MVC_CRUD_WITH_ENTITY_FRAMEWORK.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_CRUD_WITH_ENTITY_FRAMEWORK.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModels db = new DBModels())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Employee());
+            else
+            {
+                using (DBModels db = new DBModels())
+                {
+                    return View(db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            using (DBModels db = new DBModels())
+            {
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# ASP.NET MVC CRUD WITH ENTITY FRAMEWORK
+
+* Purpose: My Project
+* Used IDE: MS Visual Studio 2019
+* Target Framework: .Net Framework 4.7.2
+* Web Application Framework: ASP.Net
+* ASP.Net Programming Models: MVC (version 5.1.1)
+* Front End: HTML5, CSS3, JavaScript, AJAX, Bootstrap 3.3.7, jQuery 1.12.4, Data Tables 1.10.15
+* Back End: C#, Entity Framework 6.0.0, MS SQL Server 2017
+* Responsive Web Design: Yes
+* Type of Website: Dynamic
+
+<h2> User Interface Screenshot </h2> 
+  <img src="SCREENSHOT/PIC1.png">
+    
